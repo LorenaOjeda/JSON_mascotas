91db8d3d0767a46ed0bb3d286f9651db198549d0
Lorena Ojeda <83523132+LorenaOjeda@users.noreply.github.com>
2022-05-05 18:39:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+document.querySelector('#btnAgregarMascota').addEventListener('click', guardarMascota);
+
+function guardarMascota(){
+    var nombreM = document.querySelector('#nombreM').value,
+	 raza = document.querySelector('#raza').value,
+	 color = document.querySelector('#color').value,
+	 peso = document.querySelector('#peso').value,
+	 nombreP = document.querySelector('#nombreP').value;
+
+agregarMascota(nombreM,raza,color,peso,nombreP);
+
+//formulario.reset();
+
+}
+
+//Agregando mascota
+var mascotasLista = [];
+
+function agregarMascota(nombreM,raza,color,peso,nombreP){
+
+	var nuevaMascota = {
+		nombreM : nombreM,
+		raza : raza,
+		color : color,
+		peso : peso,
+		nombreP : nombreP
+	};
+        console.log(nuevaMascota);
+
+		mascotasLista.push(nuevaMascota);
+        
+        //convirtiendo un objeto o valor de JavaScript en una cadena de texto JSON con método stringyfy
+        var MascotaJSON = JSON.stringify(nuevaMascota);
+        
+        document.getElementById('mascotaRegistrada').innerHTML = MascotaJSON;
+
+}
+
+
+//Tratando de convertir JSON a objeto de JS con método parse
+document.querySelector('#btnAgregarMascota2').addEventListener('click', convertirAobjJS);
+
+function convertirAobjJS(){
+    var cadenaJSON  = document.querySelector('#cadenaJSON').value;
+
+    var objetoJS = JSON.parse(cadenaJSON);
+    
+    document.getElementById('mascotaRegistrada2').innerHTML = objetoJS;
+    
+       
+};
+
+
+
